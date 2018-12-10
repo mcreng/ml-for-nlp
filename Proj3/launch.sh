88615d9346297046872b9d3372124bd305bdcbed
mcreng <tsehonam@gmail.com>
2018-12-11 00:04:18 +0800

    Updated launching scripts to support time and batchsize

--- Proj3/launch.sh
+++ Proj3/launch.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+now=$(date +'%Y-%m-%d %R:%S')
 echo "Launching a new job with the following configurations."
 echo "Job Type: $1"
 echo "Epochs: $2"
 echo "Dropout: $3"
 echo "Hidden: $4"
 echo "Embed: $5"
+echo "Batch: $6"
 echo
 echo "Starting job."
 echo
 cd ~/NewProj3
-python $1.py -epochs $2 -drop $3 -hidden_size $4 -embedding_dim $5 -saved_model models/"$1"_model_"$2"_"$3"_"$4"_"$5".h5 -student_id "$1"_"$2"_"$3"_"$4"_"$5"
-python $1.py -mode test -saved_model models/"$1"_model_"$2"_"$3"_"$4"_"$5".h5 -student_id "$1"_"$2"_"$3"_"$4"_"$5"
-python scorer.py -submission "$1"_"$2"_"$3"_"$4"_"$5"_valid_result.csv
+python $1.py -epochs $2 -drop $3 -hidden_size $4 -embedding_dim $5 -batch_size $6 -saved_model models/"$1"_model_"$2"_"$3"_"$4"_"$5"_"$6".h5 -student_id "$1"_"$2"_"$3"_"$4"_"$5"_"$6"_"$now"
+python $1.py -mode test -saved_model models/"$1"_model_"$2"_"$3"_"$4"_"$5"_"$6".h5 -student_id "$1"_"$2"_"$3"_"$4"_"$5"_"$6"_"$now"
+python scorer.py -submission "$1"_"$2"_"$3"_"$4"_"$5"_"$6"_"$now"_valid_result.csv
--- Proj3/launch_all.sh
+++ Proj3/launch_all.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-job=cnn
-for epoch in 1 2 3 4 
+job=$1
+for epoch in 25 
 do
-   for dropout in `seq 0.7 0.1 0.9`
+   for dropout in 0.2 0.5 
    do
-      for hidden in 500
+      for hidden in 128 256 
       do
-         for embed in 100
+         for embed in 128 256
          do
-            qsub -F "$job $epoch $dropout $hidden $embed" launch.sh -N "$job"-"$epoch"-"$dropout"-"$hidden" -e error-"$job"-"$epoch"-"$dropout"-"$hidden"-"$embed".log -o output-"$job"-"$epoch"-"$dropout"-"$hidden"-"$embed".log -l walltime=12:00:00
+            for batch in 32
+            do
+               now=$(date +"%Y-%m-%d-%R:%S:%N")
+               qsub -F "$job $epoch $dropout $hidden $embed $batch" launch.sh -N "$job"-"$epoch"-"$dropout"-"$hidden"-"$batch" -e error-"$job"-"$epoch"-"$dropout"-"$hidden"-"$embed"-"$batch"-"$now".log -o output-"$job"-"$epoch"-"$dropout"-"$hidden"-"$embed"-"$batch"-"$now".log -l walltime=12:00:00
+            done
          done
       done
    done
